7b5b50379a97ad8e9773d4e9aacc5c5c6fecd3be
Febrialdy <aditya11febrialdy@gmail.com>
2021-01-04 02:03:18 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import TodoItems from './TodoItems.js';
+import './TodoList.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  
+  addItem(e){
+    if(this._inputElement.value !== ""){
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+      this.setState((prevState) =>{
+        return { items: prevState.items.concat(newItem) };
+      })
+      console.log(this.state.items.length);
+      this._inputElement.value = ""
+    }       
+    e.preventDefault();
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(item => {
+      return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a} placeholder="Enter Task"/>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items} delete={this.deleteItem}/>
+      </div>
+    )
+  }
 }
+
 
 export default App;
